e4fe3521ac338b10fb8f18ef1b8d198a7ca1c265
Chris Sanders <astericky@gmail.com>
2019-05-17 11:02:39 -0700

    Initial Commit

---
+++ GoodService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodService
+//
+//  Created by Christopher Sanders on 5/17/19.
+//  Copyright © 2019 Christopher Sanders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
